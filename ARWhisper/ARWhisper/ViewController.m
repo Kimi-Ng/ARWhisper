8f32418abd9acc94b5d5fd59e48969d314096504
Kimi <kiming@yahoo-inc.com>
2015-12-09 23:09:11 +0800

    First commit - add project

---
+++ ARWhisper/ARWhisper/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ARWhisper
+//
+//  Created by Cheng-Yuan Wu on 12/9/15.
+//  Copyright © 2015 Kimi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
